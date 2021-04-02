00cfe87fb1126725b2f296a064a6a760ab504e91
Mostafa Abdelrahman <43328278+Mustafa-Abdelrahman@users.noreply.github.com>
2021-04-02 18:54:58 +0200

    init

---
+++ TeamHub-MVP/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace TeamHub_MVP.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
